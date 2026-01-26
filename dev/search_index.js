b8f59f76c5bd038962f6165b90d7f77bea177e9d
Documenter.jl <documenter@juliadocs.github.io>
2026-01-26 16:42:57 +0000

    build based on 309eab2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TavaresSonoraPantanoMartinez2026.jl","page":"TavaresSonoraPantanoMartinez2026.jl","title":"TavaresSonoraPantanoMartinez2026.jl","text":"Documentation for TavaresSonoraPantanoMartinez2026.jl","category":"section"}]
+}
